5d3fc816fa995b0c5398c239511d659ca124107d
QueenMe3 <kiara1team1dream@gmail.com>
2024-01-06 17:27:35 -0600

    keepTry

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
-//import { response } from 'express'
+import { response } from 'express'
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
@@ -9,7 +9,6 @@ const urlPeople = 'http://localhost:9009/api/people'
 const App = () => {
   const [planets, setPlanets] = useState([]);
   const [people, setPeople] = useState([]);
-  const [rendered, setRendered] = useState(false);
 
   useEffect(() => {
 
@@ -20,22 +19,23 @@ const App = () => {
   axios.get('http://localhost:9009/api/people').then(response => {
     setPeople(response.data);
   });
-  setRendered(true);
   }, []);
 
-  if (!rendered) {
-    return null;
-  }
+  const matchLukeSkywalker = (element) => {
+    return element.textContext.includes('Luke Skywalker');
+  };
 
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {planets.map(planet => (
-        <Character key={planet.id} planet={planet}/>
+        <Character key={planet.id} planet={planet}
+        textMatcher={textMatcher(matchLukeSkywalker)}/>
       ))}
       {people.map(person => (
-        <Character key={person.id} person={person}/>
+        <Character key={person.id} person={person}
+        textMatcher={textMatcher(matchLukeSkywalker)}/>
       ))}
     </div>
   );
